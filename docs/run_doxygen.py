6e22e92f0fe76d5c7c7e423ae61bc10b1ed3af19
Benn Bollay <benn>
2018-03-21 22:31:21 -0700

    Add missing file

---
+++ docs/run_doxygen.py
@@ -0,0 +1,25 @@
+import subprocess, os, sys
+
+# -- Doxygen Integration through Breath --------------------------------------
+
+def run_doxygen(repo):
+    """Run the doxygen make command in the designated folder"""
+
+    try:
+        rc = subprocess.call("git clone git@github.com:vm6502q/{0}.git /tmp/{0}".format(repo), shell=True)
+        if rc < 0:
+            sys.stderr.write("failed to clone repo {0}: {1}, {2}".format(repo))
+            return
+    except OSError as e:
+        sys.stderr.write("git clone {0} failed: {1}".format(repo, e))
+        return
+
+
+    try:
+        retcode = subprocess.call("cd /tmp/{0}; doxygen doxygen.config".format(repo), shell=True)
+        if retcode < 0:
+            sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
+            return
+    except OSError as e:
+        sys.stderr.write("doxygen run for {0} failed: {1}".format(repo, e))
+        return
