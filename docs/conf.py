dfbd6eeb9c77d1db573cd9a1229df9a955ddeafb
Daniel Strano <stranoj@gmail.com>
2023-12-04 11:33:19 -0500

    Fix dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -51,8 +51,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'breathe',
-    'sphinx-rtd-theme'
+    'breathe'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
