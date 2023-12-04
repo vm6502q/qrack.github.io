3336fc5e094059d47fb6f904d3e3aa502b09df40
Daniel Strano <stranoj@gmail.com>
2023-12-04 11:19:45 -0500

    Fix dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'breathe',
+    'graphviz',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
